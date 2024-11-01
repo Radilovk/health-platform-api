612400a7e5040bfa63a0033856990d801da42fc2
Radilovk <radilov.k@gmail.com>
2024-11-01 13:29:01 +0200

    Добавяне на примерен файл за обработка на разговори с проверка на conversationId

---
+++ example_conversation.js
@@ -0,0 +1,9 @@
+// Primery javascript fail za validacia na conversationId i zarpezine na raxediata.
+function loadConversation(conversationId) {
+    if (!conversationId) {
+        alert("Conversation ID is missing or undefined.");
+        return;
+    }
+
+    // Primer API zapyt komsul"parsing ak javata za V probrem.r sam control from problok.c unes action roc size.
+function analyze to value.up config iC
