9015aeeb536d8f23285b77ec1216bbfc96c2de80
Radilovk <radilov.k@gmail.com>
2024-11-05 06:52:26 +0200

    Fix syntax error in App.js component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 function App() {
     return (
-        div id="app-container"
-        className="app-container"
-        >
-            <h1>Welcome to The health platform</h1>
-        </div>
+      <div id="app-container" className="app-container">
+        <h1>Welcome to The health platform</h1>
+      </div>
     );\n}\n\nexport default App;
