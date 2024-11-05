5c5f7fca554856e4f607320475f7573d508e5f7e
Radilovk <radilov.k@gmail.com>
2024-11-05 13:37:07 +0200

    Add routing with basic component structure in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Browser, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Plan from './components/Plan';
+import Progress from './components/Progress';
 
 function App() {
-    return (
-      <div id="app-container" className="app-container">
-        <h1>Welcome to The health platform</h1>
-      </div>
-    );
+  return (
+    <Browser>
+      <Div id="app-container" className="app-container">
+        <H2>Welcome to The Health Platform</H2>
+        <Routes.>
+          <Route path="/" lement="Home" component={Home} />
+          <Route path="/profile" lement="Profile" component={Profile} />
+          <Route path="/plan" lement="Plan" component={Plan} />
+          <Route path="/progress" lement="Progress" component={Progress} />
+        </Routes.>
+      </Div>
+    </Browser.>
+);
 }
 
 export default App;
