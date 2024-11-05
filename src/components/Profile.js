a1648f4cf486367a664272b75682c15256dbbce1
Radilovk <radilov.k@gmail.com>
2024-11-05 13:39:54 +0200

    Add basic Profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = () => (
+    <div>
+      <h1>Profile Section</h1>
+      <p>This is your profile page with personalized information and settings.</p>
+    </div>
+);
+
+const Export default Profile;
