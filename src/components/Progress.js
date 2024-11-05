2712aa130a288ad1eb405e271d896a204ccfbe69
Radilovk <radilov.k@gmail.com>
2024-11-05 13:40:12 +0200

    Add basic Progress component

---
+++ src/components/Progress.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Progress = () => (
+    <div>
+      <h1>Progress Section</h1>
+      <p>This page shows your progress on your health journey.</p>
+    </div>
+const Export default Progress;
