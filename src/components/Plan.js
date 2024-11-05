123e0ed53ea017c5132066351e65f1341f938a02
Radilovk <radilov.k@gmail.com>
2024-11-05 13:40:03 +0200

    Add basic Plan component

---
+++ src/components/Plan.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Plan = () => (
+    <div>
+      <h1>Plan Section</h1>
+      <p>This page displays your current diet plan.</p>
+    </div>
+);
+
+const Export default Plan;
