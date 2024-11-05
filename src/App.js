eb7102107da96eb466a3b25f272b919b5e15fed9
Radilovk <radilov.k@gmail.com>
2024-11-05 06:47:33 +0200

    Add App.js as main component for React application

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+    return (
+        div id="app-container"
+        className="app-container"
+        >
+            <h1>Welcome to The health platform</h1>
+        </div>
+    );\n}\n\nexport default App;
