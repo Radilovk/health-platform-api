30ebfd08679185dc21947b9b55ca03e275947d13
Radilovk <radilov.k@gmail.com>
2024-11-05 13:39:42 +0200

    Add basic Home component

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+      <h1>Home Section</h1>
+      <p>Welcome to the health platform. Please select an option from the menu to continue.</p>
+    </div>
+const Export default Home;
