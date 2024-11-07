8ab43b53fd4a5b7c5bfc51e6088d282011180fb4
Radilovk <radilov.k@gmail.com>
2024-11-07 03:04:40 +0200

    Add Part 1 of API controller (imports and routes setup)

--- api/api.js
+++ api/api.js
@@ -1,1 +1,11 @@
-// Main api controller for the project
+
+const express = require('express');
+const router = express.Router();
+
+// Import sub-controllers
+const authController = require('./auth');
+const userController = require('./userController');
+const progressController = require('./progressController');
+const dietController = require('./dietController');
+const productsController = require('./productsController');
+const articlesController = require('./articlesController');
