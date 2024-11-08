544ce2f658a1611c92ebc54f6648123afe8e4a24
Radilovk <radilov.k@gmail.com>
2024-11-09 01:28:05 +0200

    Add actionsGPT.js for bot actions handling

---
+++ bot/actionsGPT.js
@@ -0,0 +1,6 @@
+// This file contains actions for automated bot functions
+short_reminder = () => {
+  console.log('Time to register your updates!');
+};
+// Example action which can be used to provide personal updates
+module.exports = { short_reminder };
