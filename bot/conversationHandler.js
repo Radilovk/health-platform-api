d281f72885e1fbf5a7e9a2ec8edf8e4cf227f196
Radilovk <radilov.k@gmail.com>
2024-11-09 01:42:59 +0200

    Add conversationHandler.js for handling bot interactions

---
+++ bot/conversationHandler.js
@@ -0,0 +1,7 @@
+// This script handles conversations and user messages with the bot
+const processUserMessage = (message)=> {
+  // This function will anotate user messages, and call the appropriate bot functions
+  console.log("processing message: " + message);
+};
+
+exports = { processUserMessage };
