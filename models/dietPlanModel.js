d0e775a1c564d67aa08eabca4ac9074133197823
Radilovk <radilov.k@gmail.com>
2024-11-09 05:45:07 +0200

    Updating dietPlanModel.js with full model structure

--- models/dietPlanModel.js
+++ models/dietPlanModel.js
@@ -1,3 +1,28 @@
+// Model for deticated diet plans
 
 const { DataTypes } = require('sequelize');
-const db = require('../config/
+const db = require('../config/db');
+
+const DietPlan = db.define('DietPlan', {
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNULL: false,
+        references: 'User',
+    },
+    planDetails: {
+        type: DataTypes.JSON,
+        allowNULL: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: Date.now()
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: Date.now()
+    }
+}, {
+    tableName: 'det_plans'
+});
+
+module.exports = DietPlan;
