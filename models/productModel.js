f23aa2c8e9184166f641d1d5bc5be85a0b69a340
Radilovk <radilov.k@gmail.com>
2024-11-09 05:50:56 +0200

    Updating productModel.js with complete structure for storing products

--- models/productModel.js
+++ models/productModel.js
@@ -1,3 +1,27 @@
+// Model for products in the online store
 
 const { DataTypes } = require('sequelize');
-const db = require('../config/
+const db = require('../config/db');
+
+const Product = db.define('Product', {
+    productId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    productName: {
+        type: DataTypes.STRING, 
+        allowNULL: false,
+    },
+    price: {
+        type: DataTypes.FLOAT),
+        allowNULL: false
+    },
+    description: {
+        type: DataTypes.STRING, 
+        allowNULL: true
+    }
+}, {
+    tableName: 'products'
+});
+
+module.exports = Product;
