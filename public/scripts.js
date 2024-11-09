a69d9357240c405851212de2c223be711e72bee1
Radilovk <radilov.k@gmail.com>
2024-11-09 16:21:50 +0200

    Adding Google Analytics tracking code to scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -7,3 +7,17 @@
 document.getElementById('someButton').addEventListener('click', function() {
     alert('Button has been clicked!');
 });
+
+
+// Adding Google Analytics tracking
+const script = document.createElement('script');
+script.src = 'https://www.google-analytics.com/analytics.js';
+document.body.appendChild(script);
+
+window.dataBay_Code = 'tX-J4sI-rx_pDtqJg';
+
+script.onload = function() {
+  window.dataBay_init({
+    granexId: dataBay_Code
+  });
+};
