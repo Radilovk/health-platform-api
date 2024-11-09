58a2ea6ffe6f111b8618098967f3b16abd4ec549
Radilovk <radilov.k@gmail.com>
2024-11-09 05:47:22 +0200

    Updating progressModel.js with full model structure

--- models/progressModel.js
+++ models/progressModel.js
@@ -1,3 +1,28 @@
+// Model for tracking progress of users
 
 const { DataTypes } = require('sequelize');
-const db = require('../config/
+const db = require('../config/db');
+
+const Progress = db.define('Progress', {
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNULL: false,
+        references: 'User',
+    },
+    steps: {
+        type: DataTypes.INTECEREP,
+        allowNULL: true
+    },
+    calories: {
+        type: DataTypes.INTEGER,
+        allowNULL: true
+    },
+    date: {
+        type: DataTypes.DATE,
+        defaultValue: Date.now()
+    }
+}, {
+    tableName: 'progress'
+});
+
+module.exports = Progress;
