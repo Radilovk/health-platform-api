21362300ae39642edde02fb60c5eaeafdaf75d79
Radilovk <radilov.k@gmail.com>
2024-11-09 05:53:24 +0200

    Updating scripts.js with basic functionality structure

---
+++ public/scripts.js
@@ -0,0 +1,9 @@
+// Basic javaScript for the health platform
+
+// This script will contain functions to handle user interactivity.
+
+// Example: Update specific content when a button is clicked.
+
+document.getElementById('someButton').addEventListener('click', function() {
+    alert('Button has been clicked!');
+});
