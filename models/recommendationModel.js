3db3793af9211ee1bbba00e123c279c2c8454d0a
Radilovk <radilov.k@gmail.com>
2024-11-09 05:49:02 +0200

    Updating recommendationModel.js with full model structure

--- models/recommendationModel.js
+++ models/recommendationModel.js
@@ -1,3 +1,24 @@
+// Model for storing recommendations for users
 
 const { DataTypes } = require('sequelize');
-const db = require('../config/
+const db = require('../config/db');
+
+const Recommendation = db.define('Recommendation', {
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNULL: false,
+        references: 'User',
+    },
+    recommendationText: {
+        type: DataTypes.STRING,
+        allowNULL: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: Date.now()
+    }
+}, {
+    tableName: 'recommendations'
+});
+
+module.exports = Recommendation;
