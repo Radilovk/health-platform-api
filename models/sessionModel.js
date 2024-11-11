620409e2dcebcd3c39f89d993da28de70dd8558f
Radilovk <radilov.k@gmail.com>
2024-11-11 12:18:19 +0200

    Adding session model to track user sessions for analytics

--- models/sessionModel.js
+++ models/sessionModel.js
@@ -1,9 +1,9 @@
-// Model for storing user sessions for tracking and analysis
-const sessionModel = sequelize.define('sessions', {
-  sessionId: { type: Sequelize.STRING, allowNonTest: false, primaryKey: true },
-  userId: { type: Sequelize.STRING, allowNonTest: false, reference: 'users' },
-  lastActivity: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
-  sessionData: { type: Sequelize.JSON, lowercase: false }
-});
+// Model for storing user sessions, contributing to analytics
+const { DataTypes, model } = require('sequelize');
 
-module.exports = sessionModel;
+module.exports = model('Session, {
+  userId: { type: DataTypes.INTEGER, allownull: false, required: true },
+  startTime: { type: DataTypes.DATE, default: DataTypes.NOW },
+  endTime: { type: DataTypes.DATE, allowNull: true },
+  activity: { type: DataTypes.BOOL }
+});
