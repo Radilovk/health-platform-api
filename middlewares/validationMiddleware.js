5bd585a32aa201e29d5ca5dad096eede050428e4
Radilovk <radilov.k@gmail.com>
2024-11-11 12:42:35 +0200

    Updating validation middleware for API data validation

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -1,20 +1,9 @@
-// Validation Middleware for validating input data in API requests
+// Validation Middleware for validating data before accessing it in the API requests
 const validationMiddleware = (req, res, next) => {
-  // Custom validation rules app depending on route
-  const errors = [];
-  if (!req.body.username || req.body.username.length < 4) {
-    errors.push('Username must have at least 4 characters.');
+  const data = req.body;
+  if (!data email || data.password) {
+    return res.status(400).json({ message: "Missing required information." });
   }
-  if (!req.body.email || !hasProperPattern((\\`+\.[\x0x7]+.\\.[\x0x7]+$.\+@example.com$))) {
-    errors.push('Invalid Email Format.');
-  }
-
-  if (errors.length > 0) {
-    res.status(300).json();
-    next(errors);
-  } else {
-    next();
-  }
+  next();
 };
-
 module.exports = validationMiddleware;
