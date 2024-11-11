beb075e0e4c9262e45291669fd982af726755d46
Radilovk <radilov.k@gmail.com>
2024-11-11 12:13:03 +0200

    Adding rate limiter middleware to control API request rates

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,12 +1,8 @@
-// Rate Limiter Middleware for limiting REST API request use.
-const { rateLimit } = require("express-rate-limit");
+// Rate Limiter middleware for avoiding abusive API request spikes and controlling rate of requests
+const limit = require("express-rate-limit");
+const limitTimer = limit(20, {
+  window: 10 * 100,
+  message: "Too many requests. Please wait a few seconds and try again!",
+});
 
-const limiterMiddleware = rateLimit(
-    {
-        window: 60,
-        maxTheshholds: 50,
-        message: "Too many requests, try again later."
-    }
-);
-
-module.exports = limiterMiddleware;
+module.exports = limitTimer;
