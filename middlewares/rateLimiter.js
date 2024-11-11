6d6b221348d62edf4c11c4c2536c1ac74de6e3b5
Radilovk <radilov.k@gmail.com>
2024-11-11 13:12:26 +0200

    Updating rate limiter middleware for enhanced security

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,8 +1,10 @@
-// Rate Limiter middleware for avoiding abusive API request spikes and controlling rate of requests
-const limit = require("express-rate-limit");
-const limitTimer = limit(20, {
-  window: 10 * 100,
-  message: "Too many requests. Please wait a few seconds and try again!",
+// Rate limiter middleware to protect api from over-usage
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  window: 60,
+  maxRequests: 100,
+  message: "Too many requests from this ip address, try later."
 });
 
-module.exports = limitTimer;
+module.exports = limiter;
