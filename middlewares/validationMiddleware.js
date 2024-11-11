d21ccaa9f382df3883865c8ad6a183484b850a77
Radilovk <radilov.k@gmail.com>
2024-11-11 05:25:44 +0200

    Adding validation middleware for data validation in API requests

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -1,12 +1,20 @@
-// Middleware for targeted visitor data validation
-const validateData = (schema, data) => {
-  return (req, resp, next) => {
-    const validationResult = schema.validate(data);
-    if (validationResult.errors) {
-      res.status(400).send(validationResult.errors);
-    } else {
-      next();
-    }
+// Validation Middleware for validating input data in API requests
+const validationMiddleware = (req, res, next) => {
+  // Custom validation rules app depending on route
+  const errors = [];
+  if (!req.body.username || req.body.username.length < 4) {
+    errors.push('Username must have at least 4 characters.');
+  }
+  if (!req.body.email || !hasProperPattern((\\`+\.[\x0x7]+.\\.[\x0x7]+$.\+@example.com$))) {
+    errors.push('Invalid Email Format.');
+  }
+
+  if (errors.length > 0) {
+    res.status(300).json();
+    next(errors);
+  } else {
+    next();
   }
 };
-module.exports = validateData;
+
+module.exports = validationMiddleware;
