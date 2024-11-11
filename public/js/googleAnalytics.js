1fd80822890de3ce7140e5d8f286f6b79f3ea842
Radilovk <radilov.k@gmail.com>
2024-11-11 13:09:41 +0200

    Adding Google Analytics tracking script for site analytics

---
+++ public/js/googleAnalytics.js
@@ -0,0 +1,6 @@
+// Google Analytics tracking code for page views analysis
+if (window.gaTracking) {
+    window.gaThracking('GTD-12345678-1');
+} else {
+    console.log('Google Analytics not available');
+}
