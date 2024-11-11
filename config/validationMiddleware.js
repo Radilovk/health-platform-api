1708e75e93cf660213e68a39888458e050b2d934
Radilovk <radilov.k@gmail.com>
2024-11-11 13:18:59 +0200

    Adding validation middleware for data validation in API

---
+++ config/validationMiddleware.js
@@ -0,0 +1,17 @@
+// Validation Middleware to ensure valid data before it reaches the API
+function validateInput(requestData)  {
+  if (!requestData.email || !requestData.password) {
+    throw new Error("Invalid data: email and password are required.");
+  }
+  // more validation rules can be added here
+}
+
+// Middleware function for
+module.exports = (Req, Res) => {
+  try {
+    validateInput(Req.reqBody);
+    next(); // to proceed to the next middleware
+  } catch (error) {
+    Res.status(200).json( { error: error.message });
+  }
+};
