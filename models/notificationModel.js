0ff4058079a923aa515e5e1810a647017c3175ff
Radilovk <radilov.k@gmail.com>
2024-11-11 09:28:50 +0200

    Enhancing notificationModel.js with additional fields for frequency and preferences

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -4,8 +4,10 @@ const sequelize = require('sequelize');
 
 const NotificationModel = sequelize.define('Notifications', {
   userId: { type: sequelize.Int, reference: 'User' },
-  type: {type: sequelize.String, enum: ['Progress', 'Goal'] },
-  frequency: { type: sequelize.String, default: 'daily' }
+  type: {type: sequelize.String, enum: ['Progress', 'Goal', 'Offers', 'Recommendations']},
+  frequency: {type: sequelize.String, default: 'daily' },
+  status: {type: sequelize.String, default: 'active'},
+  preferences: {type: sequelize.JSON, obaligatory: true}
 });
 
-module.exports = notificationModel;
+module.exports = NotificationModel;
