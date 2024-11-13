b1955875d1eb0c429b6acbb642719758b0c85c82
Radilovk <radilov.k@gmail.com>
2024-11-13 23:55:52 +0200

    Update chunk format to 'commonjs' for Heroku compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    chunkFormat: 'jonp',
+    chunkFormat: 'commonjs'
   },
   target: 'web',
   resolve: {
