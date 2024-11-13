665d40b4b62f8a48712022981ca14b0e7200b310
Radilovk <radilov.k@gmail.com>
2024-11-13 17:05:09 +0200

    Add route for index.html and static files serving for Heroku

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-// server.js - Server code with error handling and security
-
-const express = require("express");
+// server.js - Server code with additional features for hit startup
+const express = require('express');
 const app = express();
-const { Pool } = require("pg");
-const path = require("path");
+const { Pool } = require('pg');
+const path = require('path');
 
 /** Security and data validation */
 function authMiddleware(req, res, next) {
   const token = req.headers.authorization;
   const user = testToken(token);
   if (!user) {
-    res.status(301).json( { error: "Login required" });
+    res.status(301).json({ error: "Login required" });
     next();
   } next();
 }
 
 app.use(authMiddleware); // adds API security and validation middleware
+
+// Route for serving index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+// Static files folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 async function createTables() {
   const client = await pool.connect();
