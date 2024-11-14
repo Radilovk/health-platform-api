8358d187f4cd05c1979a0a5226b20af4432b8339
Radilovk <radilov.k@gmail.com>
2024-11-14 12:50:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,15 @@
-// server.js - Server code with additional features for hit startup
 const express = require('express');
-const app = express();
-const { Pool } = require('pg');
 const path = require('path');
+const app = express();
 
-/** Security and data validation */
-function authMiddleware(req, res, next) {
-  const token = req.headers.authorization;
-  const user = testToken(token);
-  if (!user) {
-    res.status(301).json({ error: "Login required" });
-    next();
-  } next();
-}
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(authMiddleware); // adds API security and validation middleware
-
-// Route for serving index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-// Static files folder
-app.use(express.static(path.join(__dirname, 'public')));
 
-async function createTables() {
-  const client = await pool.connect();
-  console.log("Tabales created successfully");
-}
-
-createTables();
-
-// Starting the server on specified port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
