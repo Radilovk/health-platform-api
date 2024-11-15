00d4c20fc7501da32772ddd61a0a579d01716fed
Radilovk <radilov.k@gmail.com>
2024-11-15 05:36:31 +0200

    Configure css-loader and style-loader in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,13 @@ module.exports = {
       { test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      { test: /\\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.css']
   }
 };
